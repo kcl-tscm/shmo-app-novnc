5ac19df00f8b4da04d8122dc919fef7352ea564c
Adam Fekete <adam.fekete@kcl.ac.uk>
2018-09-30 20:25:56 +0100

    Dockerfile with novnc support

---
+++ rootfs/root/startapp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Xvfb $DISPLAY -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x24 &
+sleep 5
+openbox-session &
+x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+
+ln -s /usr/share/novnc/vnc_auto.html /usr/share/novnc/index.html
+websockify -D --web=/usr/share/novnc/ 5800 localhost:5900
+
+
+# Make sure some directories are created.
+mkdir -p /root/config/downloads
+mkdir -p /root/config/log/firefox
+mkdir -p /root/config/profile
+
+# Generate machine id.
+if [ ! -f /etc/machine-id ]; then
+    echo "generating machine-id..."
+    cat /proc/sys/kernel/random/uuid | tr -d '-' > /etc/machine-id
+fi
+
+/root/firefox/firefox --profile /root/config/profile --setDefaultBrowser /root/SHMo2/SHMo2.html
+
+
